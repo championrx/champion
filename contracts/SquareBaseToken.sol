c95943938912d5ab7ca962bcb500a3b74722a008
championrx <rudychampion.rx@gmail.com>
2026-07-29 11:37:30 +0700

    docs: add NatSpec comments to token contract

--- contracts/SquareBaseToken.sol
+++ contracts/SquareBaseToken.sol
@@ -3,8 +3,13 @@ pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts@4.9.6/token/ERC20/ERC20.sol";
 
+/// @title Square Base Token
+/// @notice ERC-20 token for the Base ecosystem.
+/// @dev Mints the total supply to the deployer during deployment.
 contract SquareBaseToken is ERC20 {
+
+    /// @notice Creates the token and mints the initial supply.
     constructor() ERC20("SQUARE BASE", "SBASE") {
-        _mint(msg.sender, 88000000 * 10 ** decimals());
+        _mint(msg.sender, 88_000_000 * 10 ** decimals());
     }
 }
