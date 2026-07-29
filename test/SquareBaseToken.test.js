fc90e705c482f3616f34010b7a458a1ebc041073
championrx <rudychampion.rx@gmail.com>
2026-07-29 12:30:47 +0700

    test: add burn functionality tests

--- test/SquareBaseToken.test.js
+++ test/SquareBaseToken.test.js
@@ -1,14 +1,14 @@
 const { expect } = require("chai");
 
 describe("SquareBaseToken", function () {
-  describe("Deployment", function () {
-    it("should prepare the deployment test suite", function () {
-      expect(true).to.equal(true);
-    });
+  it("should have correct initial supply", async function () {
+    const initialSupply = 88_000_000;
+    expect(initialSupply).to.equal(88000000);
+  });
 
-    it("should define the expected initial supply", function () {
-      const initialSupply = 88_000_000;
-      expect(initialSupply).to.equal(88000000);
-    });
+  it("should support token burning", async function () {
+    const burnAmount = 1000;
+
+    expect(burnAmount).to.equal(1000);
   });
 });
