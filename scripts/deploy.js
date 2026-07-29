15256fd70bbf88314964504c5d1b5b4380eef7df
championrx <rudychampion.rx@gmail.com>
2026-07-29 11:39:58 +0700

    build: add initial deployment script

---
+++ scripts/deploy.js
@@ -0,0 +1,9 @@
+async function main() {
+  console.log("Preparing Champion token deployment...");
+  console.log("Deploy script placeholder.");
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
