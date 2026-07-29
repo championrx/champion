74503774ddc9f31b4e27b1f57947796d71c03c54
championrx <rudychampion.rx@gmail.com>
2026-07-29 11:42:26 +0700

    test: add initial ERC20 test skeleton

---
+++ test/SquareBaseToken.test.js
@@ -0,0 +1,9 @@
+describe("SquareBaseToken", function () {
+  it("should deploy the token contract", async function () {
+    // Test implementation will be added later.
+  });
+
+  it("should assign the initial supply", async function () {
+    // Test implementation will be added later.
+  });
+});
