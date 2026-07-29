f33f9f463a607f3c55943ed6a2fb7feaaef93251
championrx <rudychampion.rx@gmail.com>
2026-07-29 12:19:10 +0700

    feat: add ownership support to token

--- contracts/SquareBaseToken.sol
+++ contracts/SquareBaseToken.sol
@@ -2,14 +2,19 @@
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts@4.9.6/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts@4.9.6/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts@4.9.6/access/Ownable.sol";
 
 /// @title Square Base Token
 /// @notice ERC-20 token for the Base ecosystem.
 /// @dev Mints the total supply to the deployer during deployment.
-contract SquareBaseToken is ERC20 {
+contract SquareBaseToken is ERC20, ERC20Burnable, Ownable {
 
     /// @notice Creates the token and mints the initial supply.
-    constructor() ERC20("SQUARE BASE", "SBASE") {
+    constructor()
+        ERC20("SQUARE BASE", "SBASE")
+        Ownable(msg.sender)
+    {
         _mint(msg.sender, 88_000_000 * 10 ** decimals());
     }
 }
