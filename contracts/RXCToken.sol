a1405ba45c215d5b028ee7a4a0211c4667ab0830
championrx <rudychampion.rx@gmail.com>
2026-08-03 12:21:28 +0700

    feat: add RXC burnable feature

--- contracts/RXCToken.sol
+++ contracts/RXCToken.sol
@@ -2,9 +2,10 @@
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract RXCToken is ERC20, Ownable {
+contract RXCToken is ERC20, ERC20Burnable, Ownable {
     uint256 public constant INITIAL_SUPPLY = 100_000_000 * 10 ** 18;
 
     constructor() ERC20("RXC Token", "RXC") Ownable(msg.sender) {
