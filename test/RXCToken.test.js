45431729be21a978306dd3a955080563cc5576ed
championrx <rudychampion.rx@gmail.com>
2026-08-03 12:23:22 +0700

    test: add RXC burn function test

--- test/RXCToken.test.js
+++ test/RXCToken.test.js
@@ -17,3 +17,19 @@ describe("RXCToken", function () {
     expect(await token.balanceOf(owner.address)).to.equal(supply);
   });
 });
+  it("Should allow token burning", async function () {
+    const [owner] = await ethers.getSigners();
+
+    const RXCToken = await ethers.getContractFactory("RXCToken");
+    const token = await RXCToken.deploy();
+
+    const burnAmount = ethers.parseEther("1000");
+
+    await token.burn(burnAmount);
+
+    const balance = await token.balanceOf(owner.address);
+
+    expect(balance).to.equal(
+      ethers.parseEther("99999000")
+    );
+  });
