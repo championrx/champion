34d403037de7e19609b61a830641ad8ef70e7d0f
championrx <rudychampion.rx@gmail.com>
2026-08-03 12:25:13 +0700

    feat: add RXC pause functionality

--- contracts/RXCToken.sol
+++ contracts/RXCToken.sol
@@ -3,12 +3,29 @@ pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract RXCToken is ERC20, ERC20Burnable, Ownable {
+contract RXCToken is ERC20, ERC20Burnable, Pausable, Ownable {
     uint256 public constant INITIAL_SUPPLY = 100_000_000 * 10 ** 18;
 
     constructor() ERC20("RXC Token", "RXC") Ownable(msg.sender) {
         _mint(msg.sender, INITIAL_SUPPLY);
+    }
+
+    function pause() external onlyOwner {
+        _pause();
+    }
+
+    function unpause() external onlyOwner {
+        _unpause();
+    }
+
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal override whenNotPaused {
+        super._beforeTokenTransfer(from, to, amount);
     }
 }
