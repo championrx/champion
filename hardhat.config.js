c0357ec3199c62b9d25556cb309e04fd5ccf9b7b
championrx <rudychampion.rx@gmail.com>
2026-08-03 12:28:30 +0700

    chore: improve hardhat configuration

--- hardhat.config.js
+++ hardhat.config.js
@@ -12,3 +12,20 @@ export default {
     },
   },
 };
+require("@nomicfoundation/hardhat-toolbox");
+
+module.exports = {
+  solidity: {
+    version: "0.8.20",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+
+  networks: {
+    hardhat: {},
+  },
+};
